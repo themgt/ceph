bc22955c562d2a348c40d89121953f2fa2fb283a
John Spray <john.spray@inktank.com>
2014-02-18 13:26:16 +0000

    mds: Avoid assertions in shutdown
    MDS::suicide() was tearing down the objecter, but it was possible
    for a _dispatch call to still be in process (#5382) while that happened
    if called from a signal handler.  Fix this by checking for DNE state
    every time mds_lock is taken, including in the various places it's
    dropped during _dispatch.
    
    Fixes: #5382

--- src/mds/MDS.cc
+++ src/mds/MDS.cc
@@ -1641,10 +1641,15 @@ void MDS::handle_signal(int signum)
   mds_lock.Unlock();
 }
 
+/**
+ * Initiate shutdown.  This does not terminate the process synchronously,
+ * rather it initiates a process which leads to messenger completion so that
+ * we fall out of our dispatch loop.
+ */
 void MDS::suicide()
 {
   assert(mds_lock.is_locked());
-  want_state = CEPH_MDS_STATE_DNE; // whatever.
+  want_state = CEPH_MDS_STATE_DNE;
 
   dout(1) << "suicide.  wanted " << ceph_mds_state_name(want_state)
 	  << ", now " << ceph_mds_state_name(state) << dendl;
@@ -1913,6 +1918,12 @@ bool MDS::_dispatch(Message *m)
     }
   }
 
+  // handle_core_message may relinquish and re-acquire mds_lock
+  // to handle config setting message, so we must re-check state
+  if (want_state == CEPH_MDS_STATE_DNE) {
+	return false;
+  }
+
   // finish any triggered contexts
   while (!finished_queue.empty()) {
     dout(7) << "mds has " << finished_queue.size() << " queued contexts" << dendl;
@@ -1927,6 +1938,9 @@ bool MDS::_dispatch(Message *m)
       // give other threads (beacon!) a chance
       mds_lock.Unlock();
       mds_lock.Lock();
+      if (want_state == CEPH_MDS_STATE_DNE) {
+	return false;
+      }
     }
   }
 
@@ -1949,6 +1963,9 @@ bool MDS::_dispatch(Message *m)
     // give other threads (beacon!) a chance
     mds_lock.Unlock();
     mds_lock.Lock();
+    if (want_state == CEPH_MDS_STATE_DNE) {
+	return false;
+    }
   }
 
   // done with all client replayed requests?
@@ -2210,6 +2227,9 @@ bool MDS::ms_verify_authorizer(Connection *con, int peer_type,
 void MDS::ms_handle_accept(Connection *con)
 {
   Mutex::Locker l(mds_lock);
+  if (want_state == CEPH_MDS_STATE_DNE)
+    return;
+
   Session *s = static_cast<Session *>(con->get_priv());
   dout(10) << "ms_handle_accept " << con->get_peer_addr() << " con " << con << " session " << s << dendl;
   if (s) {
