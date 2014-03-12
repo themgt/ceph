0a523c8c10ae15cf63a7ef04f39ebb1a1c74d5d9
Greg Farnum <greg@inktank.com>
2014-03-12 13:03:26 -0700

    Locker: Switch some MutationRef& for MutationRef
    Sadly, you can't implicitly convert non-const references to shared pointers. :(
    
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/mds/Locker.cc
+++ src/mds/Locker.cc
@@ -510,7 +510,7 @@ bool Locker::acquire_locks(MDRequestRef& mdr,
 }
 
 
-void Locker::set_xlocks_done(MutationRef& mut, bool skip_dentry)
+void Locker::set_xlocks_done(MutationRef mut, bool skip_dentry)
 {
   for (set<SimpleLock*>::iterator p = mut->xlocks.begin();
        p != mut->xlocks.end();
@@ -581,7 +581,7 @@ void Locker::_drop_non_rdlocks(MutationRef& mut, set<CInode*> *pneed_issue)
   }
 }
 
-void Locker::cancel_locking(MutationRef& mut, set<CInode*> *pneed_issue)
+void Locker::cancel_locking(MutationRef mut, set<CInode*> *pneed_issue)
 {
   SimpleLock *lock = mut->locking;
   assert(lock);
@@ -602,7 +602,7 @@ void Locker::cancel_locking(MutationRef& mut, set<CInode*> *pneed_issue)
   mut->finish_locking(lock);
 }
 
-void Locker::drop_locks(MutationRef& mut, set<CInode*> *pneed_issue)
+void Locker::drop_locks(MutationRef mut, set<CInode*> *pneed_issue)
 {
   // leftover locks
   set<CInode*> my_need_issue;
@@ -619,7 +619,7 @@ void Locker::drop_locks(MutationRef& mut, set<CInode*> *pneed_issue)
   mut->done_locking = false;
 }
 
-void Locker::drop_non_rdlocks(MutationRef& mut, set<CInode*> *pneed_issue)
+void Locker::drop_non_rdlocks(MutationRef mut, set<CInode*> *pneed_issue)
 {
   set<CInode*> my_need_issue;
   if (!pneed_issue)
@@ -631,7 +631,7 @@ void Locker::drop_non_rdlocks(MutationRef& mut, set<CInode*> *pneed_issue)
     issue_caps_set(*pneed_issue);
 }
 
-void Locker::drop_rdlocks(MutationRef& mut, set<CInode*> *pneed_issue)
+void Locker::drop_rdlocks(MutationRef mut, set<CInode*> *pneed_issue)
 {
   set<CInode*> my_need_issue;
   if (!pneed_issue)
@@ -1218,7 +1218,7 @@ void Locker::nudge_log(SimpleLock *lock)
     mds->mdlog->flush();
 }
 
-void Locker::rdlock_finish(SimpleLock *lock, MutationRef& mut, bool *pneed_issue)
+void Locker::rdlock_finish(SimpleLock *lock, MutationRef mut, bool *pneed_issue)
 {
   // drop ref
   lock->put_rdlock();
@@ -1351,7 +1351,7 @@ bool Locker::wrlock_start(SimpleLock *lock, MDRequestRef& mut, bool nowait)
   return false;
 }
 
-void Locker::wrlock_finish(SimpleLock *lock, MutationRef& mut, bool *pneed_issue)
+void Locker::wrlock_finish(SimpleLock *lock, MutationRef mut, bool *pneed_issue)
 {
   if (lock->get_type() == CEPH_LOCK_IVERSION ||
       lock->get_type() == CEPH_LOCK_DVERSION)
@@ -1402,7 +1402,7 @@ void Locker::remote_wrlock_start(SimpleLock *lock, int target, MDRequestRef& mut
 }
 
 void Locker::remote_wrlock_finish(SimpleLock *lock, int target,
-                                  MutationRef& mut)
+                                  MutationRef mut)
 {
   // drop ref
   mut->remote_wrlocks.erase(lock);
@@ -1523,7 +1523,7 @@ void Locker::_finish_xlock(SimpleLock *lock, client_t xlocker, bool *pneed_issue
   eval_gather(lock, true, pneed_issue);
 }
 
-void Locker::xlock_finish(SimpleLock *lock, MutationRef& mut, bool *pneed_issue)
+void Locker::xlock_finish(SimpleLock *lock, MutationRef mut, bool *pneed_issue)
 {
   if (lock->get_type() == CEPH_LOCK_IVERSION ||
       lock->get_type() == CEPH_LOCK_DVERSION)
@@ -1578,7 +1578,7 @@ void Locker::xlock_finish(SimpleLock *lock, MutationRef& mut, bool *pneed_issue)
   }
 }
 
-void Locker::xlock_export(SimpleLock *lock, MutationRef& mut)
+void Locker::xlock_export(SimpleLock *lock, MutationRef mut)
 {
   dout(10) << "xlock_export on " << *lock << " " << *lock->get_parent() << dendl;
 
@@ -1595,7 +1595,7 @@ void Locker::xlock_export(SimpleLock *lock, MutationRef& mut)
   lock->set_state(LOCK_LOCK);
 }
 
-void Locker::xlock_import(SimpleLock *lock, MutationRef& mut)
+void Locker::xlock_import(SimpleLock *lock, MutationRef mut)
 {
   dout(10) << "xlock_import on " << *lock << " " << *lock->get_parent() << dendl;
   lock->get_parent()->auth_pin(lock);
--- src/mds/Locker.h
+++ src/mds/Locker.h
@@ -90,11 +90,11 @@ public:
 		     CInode *auth_pin_freeze=NULL,
 		     bool auth_pin_nonblock=false);
 
-  void cancel_locking(MutationRef& mut, set<CInode*> *pneed_issue);
-  void drop_locks(MutationRef& mut, set<CInode*> *pneed_issue=0);
-  void set_xlocks_done(MutationRef& mut, bool skip_dentry=false);
-  void drop_non_rdlocks(MutationRef& mut, set<CInode*> *pneed_issue=0);
-  void drop_rdlocks(MutationRef& mut, set<CInode*> *pneed_issue=0);
+  void cancel_locking(MutationRef mut, set<CInode*> *pneed_issue);
+  void drop_locks(MutationRef mut, set<CInode*> *pneed_issue=0);
+  void set_xlocks_done(MutationRef mut, bool skip_dentry=false);
+  void drop_non_rdlocks(MutationRef mut, set<CInode*> *pneed_issue=0);
+  void drop_rdlocks(MutationRef mut, set<CInode*> *pneed_issue=0);
 
   void eval_gather(SimpleLock *lock, bool first=false, bool *need_issue=0, list<Context*> *pfinishers=0);
   void eval(SimpleLock *lock, bool *need_issue);
@@ -128,24 +128,24 @@ public:
   bool _rdlock_kick(SimpleLock *lock, bool as_anon);
   bool rdlock_try(SimpleLock *lock, client_t client, Context *c);
   bool rdlock_start(SimpleLock *lock, MDRequestRef& mut, bool as_anon=false);
-  void rdlock_finish(SimpleLock *lock, MutationRef& mut, bool *pneed_issue);
+  void rdlock_finish(SimpleLock *lock, MutationRef mut, bool *pneed_issue);
   bool can_rdlock_set(set<SimpleLock*>& locks);
   bool rdlock_try_set(set<SimpleLock*>& locks);
   void rdlock_take_set(set<SimpleLock*>& locks, MutationRef& mut);
 
   void wrlock_force(SimpleLock *lock, MutationRef& mut);
   bool wrlock_start(SimpleLock *lock, MDRequestRef& mut, bool nowait=false);
-  void wrlock_finish(SimpleLock *lock, MutationRef& mut, bool *pneed_issue);
+  void wrlock_finish(SimpleLock *lock, MutationRef mut, bool *pneed_issue);
 
   void remote_wrlock_start(SimpleLock *lock, int target, MDRequestRef& mut);
-  void remote_wrlock_finish(SimpleLock *lock, int target, MutationRef& mut);
+  void remote_wrlock_finish(SimpleLock *lock, int target, MutationRef mut);
 
   bool xlock_start(SimpleLock *lock, MDRequestRef& mut);
   void _finish_xlock(SimpleLock *lock, client_t xlocker, bool *pneed_issue);
-  void xlock_finish(SimpleLock *lock, MutationRef& mut, bool *pneed_issue);
+  void xlock_finish(SimpleLock *lock, MutationRef mut, bool *pneed_issue);
 
-  void xlock_export(SimpleLock *lock, MutationRef& mut);
-  void xlock_import(SimpleLock *lock, MutationRef& mut);
+  void xlock_export(SimpleLock *lock, MutationRef mut);
+  void xlock_import(SimpleLock *lock, MutationRef mut);
 
 
   // simple
